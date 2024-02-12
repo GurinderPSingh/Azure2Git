542c8aa7425353ab915e9b1456076ff18906ad63
GurinderPSingh <139916699+GurinderPSingh@users.noreply.github.com>
2024-02-11 18:52:20 -0600

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -12,17 +12,24 @@ param(
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
 Write-Host ' reflect Azure Devops repo changes to GitHub repo'
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
+
 $AzureRepoName = "LINC_test2"
 Write-Host "Name: $AzureRepoName"
+
 $ADOCloneURL = "dev.azure.com/gpsinghLTC/LINC_test2/_git/LINC_test2"
 $GitHubCloneURL = "github.com/GurinderPSingh/LINC_test2.git"
+
 $stageDir = pwd | Split-Path
 Write-Host "stage Dir is : $stageDir"
+
 $githubDir = $stageDir +"\"+"gitHub"
 Write-Host "github Dir : $githubDir"
+
 $destination = $githubDir+"\"+ $AzureRepoName +".git"
 Write-Host "destination: $destination"
+
 #Please make sure, you remove https from azure-repo-clone-url
+
 $sourceURL = "https://$($ADOSourcePAT)"+"@"+"$($ADOCloneURL)"
 write-host "source URL : $sourceURL"
 #Please make sure, you remove https from github-repo-clone-url
