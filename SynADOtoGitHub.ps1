3021f253e90660bbe5323f3e2c173a1ed27d9c0a
GurinderPSingh <139916699+GurinderPSingh@users.noreply.github.com>
2024-02-11 18:39:51 -0600

    Update SynADOtoGitHub.ps1

--- SynADOtoGitHub.ps1
+++ SynADOtoGitHub.ps1
@@ -13,7 +13,7 @@ Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - -'
 Write-Host ' reflect Azure Devops repo changes to GitHub repo'
 Write-Host ' - - - - - - - - - - - - - - - - - - - - - - - - - '
 $AzureRepoName = "LINC_test2"
-$ADOCloneURL = "dev.azure.com/gpsinghLTC/test%20LINC/_git/test%20LINC"
+$ADOCloneURL = "dev.azure.com/gpsinghLTC/LINC_test2/_git/LINC_test2"
 $GitHubCloneURL = "github.com/GurinderPSingh/LINC_test2.git"
 $stageDir = pwd | Split-Path
 Write-Host "stage Dir is : $stageDir"
